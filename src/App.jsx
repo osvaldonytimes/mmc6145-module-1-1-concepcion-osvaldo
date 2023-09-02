0504467035864c1498ea5c067b8fa980906d10ea
Osvaldo Concepcion <osvaldoconcep@gmail.com>
2023-09-02 11:57:15 -0400

    Client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about.jsx";
+import Contact from "./pages/contact.jsx";
 
 export default function App() {
   return (
-    <>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
-  )
+    </div>
+  );
 }
