2425010e2a285c02e68a7f3e3f45bbf3f371bac0
Osvaldo Concepcion <osvaldoconcep@gmail.com>
2023-09-02 11:56:21 -0400

    client side routing and scaffolding of pages

---
+++ src/pages/about.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/about.module.css";
+
+export default function About() {
+  return (
+    <main>
+      <h1 className={styles.placeholder}>I'm the about page!</h1>
+    </main>
+  );
+}
