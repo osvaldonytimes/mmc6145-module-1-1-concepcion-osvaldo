3843dadd707501f298e67aacd537776b0791b4c1
Osvaldo Concepcion <osvaldoconcep@gmail.com>
2023-09-02 13:27:56 -0400

    Contact page content

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,7 +3,36 @@ import styles from "../styles/contact.module.css";
 export default function Contact() {
   return (
     <>
-      <h1 className={styles.placeholder}>I'm the contact page!</h1>
+      <div className={styles.contactContainer}>
+        <h1>Contact Me</h1>
+        <p>
+          I'd love to connect with you. You can find me on the following
+          platforms:
+        </p>
+
+        <ul className={styles.socialList}>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/osvaldoconcepcion/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.socialLink}
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/osvaldonytimes"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.socialLink}
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
